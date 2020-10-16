79e02f3f204a59489a7c8354102f19b726b07fcb
Adam Kiss <kiss.adam.a@gmail.com>
2020-10-16 14:35:11 +0300

    Remove "fontfaceobserver" dependency

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,11 @@
 import Head from 'next/head';
 import { useState, useCallback, useEffect } from 'react';
-import FontFaceObserver from 'fontfaceobserver';
 
 import 'normalize.css';
 import 'styles/global.scss';
 import { Header } from 'components/header';
 
 function App({ Component, pageProps }) {
-  const [haveStyleAssetsLoaded, setHaveStyleAssetsLoaded] = useState(false);
-
-  const setUpInterFontObserver = useCallback(async () => {
-    const interFontObserver = new FontFaceObserver('Inter');
-    await interFontObserver.load();
-    setHaveStyleAssetsLoaded(true);
-  }, []);
-
-  useEffect(() => {
-    setUpInterFontObserver();
-  }, [setUpInterFontObserver]);
-
   const showHeader = Component.showHeader || false;
   const currentPage = Component.pageName || 'home';
 
@@ -26,6 +13,11 @@ function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Adam Kiss | Personal</title>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com/"
+          crossOrigin=""
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
@@ -41,6 +33,15 @@ function App({ Component, pageProps }) {
         {`
           .wrapper {
             padding: ${showHeader ? '2em' : '0em'};
+          }
+          .loading-wrapper {
+            position: fixed;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            background-color: white;
+            z-index: 9999;
           }
         `}
       </style>
